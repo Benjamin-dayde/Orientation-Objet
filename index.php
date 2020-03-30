dc60b56b5899d77664f01fa586e75f7bacaff307
Benjamin-dayde <benjamin.dayde@orange.fr>
2020-03-30 11:22:35 +0200

    Fonction get et set

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 
 class Personnage {
 
-     public $nom;
-     public $force;
-     public $level;
-     public $hp;
-     public $vie; 
+     private $nom;
+     private $force;
+     private $level;
+     private $hp;
+     private $vie;
+     
+    function __construct(string $nom, int $force = 20, int $level = 1, int $hp = 100,string $vie) {
+        $this->nom = $nom;
+        $this->force = $force;
+        $this->level = $level;
+        $this->hp = $hp;
+        $this->vie = $vie;
+    }
 
     function carateristique() {
         echo $this->nom." a une force de ".$this->force." point au level ".$this->level." il a ".$this->hp." HP et il est ".$this->etat(); 
@@ -22,13 +30,56 @@ class Personnage {
         return $vie;
     }
 
+    function getNom(): string {
+        return $this->nom;
+    }
+
+    function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    function getForce(): int {
+        return $this->force;
+    }
+
+    function setForce($force) {
+        $this->force = $force;
+    }
+
+    function getLevel(): int {
+        return $this->level;
+    }
+
+    function setLevel($level) {
+        $this->level = $level;
+    }
+
+    function getHp(): int {
+        return $this->hp;
+    }
+
+    function setHp($hp) {
+        $this->hp = $hp;
+    }
+
+    function getVie(): string {
+        return $this->vie;
+    }
+
+    function setVie($vie) {
+        $this->vie = $vie;
+    }
+
 };
 
-$perso1 = new Personnage();
-$perso2 = new Personnage();
-$perso3 = new Personnage();
+$perso1 = new Personnage( "pierre",  30,  3,  100, "");
+$perso2 = new Personnage( "paul",  50,  1,  0, "");
+$perso3 = new Personnage( "jack",  30,  2,  50, "");
 
-$perso1->nom = "Pierre";
+$perso2->setNom("miguel");
+echo $perso2->getNom();
+
+/*$perso1->nom = "Pierre";
 $perso1->force = 30;
 $perso1->level = 1;
 $perso1->hp = 100;
@@ -38,12 +89,12 @@ $perso2->nom = "Paul";
 $perso2->force = 10;
 $perso2->level = 1;
 $perso2->hp = 0;
-$perso2->vie = "";
+$perso2->vie;
 
 $perso3->nom = "Jack";
 $perso3->force = 20;
 $perso3->level = 1;
 $perso3->hp = 100;
-$perso3->vie = "";
+$perso3->vie;*/
 
-$perso2->carateristique();
+$perso1->carateristique();
