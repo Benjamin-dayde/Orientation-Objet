0e7409b6a50e57c38fb7c5d19ab96c2f38041956
Benjamin-dayde <benjamin.dayde@orange.fr>
2020-03-30 13:57:50 +0200

    rajout du echo avec getter et setter

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ class Personnage {
      private $level;
      private $hp;
      private $vie;
-     
+    // construction des personage
     function __construct(string $nom, int $force = 20, int $level = 1, int $hp = 100,string $vie) {
         $this->nom = $nom;
         $this->force = $force;
@@ -15,11 +15,11 @@ class Personnage {
         $this->hp = $hp;
         $this->vie = $vie;
     }
-
+    // Affiche les carateristique //
     function carateristique() {
         echo $this->nom." a une force de ".$this->force." point au level ".$this->level." il a ".$this->hp." HP et il est ".$this->etat(); 
     }
-
+    // défini si le personnage et mort ou vivant en fonction des HP //
     function etat() {
         if($this->hp >= 1) {
             $vie = "alive";
@@ -29,7 +29,7 @@ class Personnage {
 
         return $vie;
     }
-
+    // Permet la modification des perso //
     function getNom(): string {
         return $this->nom;
     }
@@ -70,20 +70,25 @@ class Personnage {
         $this->vie = $vie;
     }
 
+    function attaquer() {
+        echo $this->nom." attaque ". setNom($nom) . setNom($nom) ." perds ".$this->force;
+    }
+
 };
+// implementation des personnage //
+$perso1 = new Personnage( "",  30,  3,  100, "");
+$perso2 = new Personnage( "",  50,  1,  0, "");
+$perso3 = new Personnage( "",  30,  2,  50, "");
 
-$perso1 = new Personnage( "pierre",  30,  3,  100, "");
-$perso2 = new Personnage( "paul",  50,  1,  0, "");
-$perso3 = new Personnage( "jack",  30,  2,  50, "");
-
+// modification des personnage //
 $perso2->setNom("robert");
 $perso2->setForce(50);
 $perso2->setLevel(10);
 $perso2->setHp(30);
 $perso2->setVie("");
 
+echo "le personnage ". $perso1->getNom() ." a ". $perso1->getForce() ." point de force et ". $perso1->getHp() ." point de vie en étant level". $perso1->getLevel() .". son état de santé indique qu'il est ". $perso1->getVie();
 
-
-echo $perso2->carateristique();
+//echo $perso2->carateristique();
 
 
