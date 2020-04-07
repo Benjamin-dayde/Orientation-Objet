9e507291e40386a3ceed996633b34555807ca661
Benjamin-dayde <benjamin.dayde@orange.fr>
2020-04-07 10:26:18 +0200

    Exo 2

--- fichier.php
+++ fichier.php
@@ -1,8 +1,28 @@
 <?php
 
-$fichier = fopen("date de mise a jour" , "c");
+$fichier = fopen("maj.txt", "r");
 
+fwrite($fichier, "date de mise a jour 10/10/97".PHP_EOL);
+fwrite($fichier, "heure de maj 00:00".PHP_EOL);
+
+$len = "";
+
+while ($len !== false) {
+    $len = fgets($fichier);
+    var_dump($len);
+}
 
 fclose($fichier);
 
+$compteur = fopen("compteur.txt", "r");
 
+$page = ["index.html", "plat.html", "entree.html" , "dessert.html"];
+
+$tab = sizeof($page);
+
+fwrite($compteur , "Vous avez ".$tab." page sur votre site ".PHP_EOL);
+
+var_dump(fgets($compteur));
+
+
+fclose($compteur);
