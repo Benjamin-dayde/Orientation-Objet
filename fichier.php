548c9fd22d64500237a16220bd737e0800978a50
Benjamin-dayde <benjamin.dayde@orange.fr>
2020-04-08 08:52:35 +0200

    creation du formulaire

--- fichier.php
+++ fichier.php
@@ -46,3 +46,66 @@ fclose($personnage);
 // exo 4 //
 
 
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+<!-- Exo 4 -->
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>formulaire</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+<section>
+            <h2>Commentaires</h2>
+            <form action="formulaire.php" method="post">
+                <p>
+                    <label for="titre">Titre de la recette</label>
+                    <input type="text" id="titre" name="titre" placeholder="">
+                </p>
+
+                <p>
+                    <label for="ingredients">Les ingredients</label>
+                    <input type="text" id="ingredients" name="ingredients" placeholder="">
+                </p>
+
+                <p>
+                    <label for="ingredients">Les étapes</label>
+                    <input type="text" id="etapes" name="etapes" placeholder="">
+                </p>
+
+                <p>Difficulté /5 : 
+                    <label for="rad0">0</label>
+                    <input type="radio" id="rad0" name="difficulte" value="0">
+                    <label for="rad1">1</label>
+                    <input type="radio" id="rad1" name="difficulte" value="1">
+                    <label for="rad2">2</label>
+                    <input type="radio" id="rad2" name="difficulte" value="2">
+                    <label for="rad3">3</label>
+                    <input type="radio" id="rad3" name="difficulte" value="3">
+                    <label for="rad4">4</label>
+                    <input type="radio" id="rad4" name="difficulte" value="4">
+                    <label for="rad5">5</label>
+                    <input type="radio" id="rad5" name="difficulte" value="5">
+                </p>
+
+                <p><input type="submit" value="Envoyer">
+                </p>
+            </form>
+        </section>
+
+</html>
+
+
